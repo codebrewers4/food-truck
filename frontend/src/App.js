a143686b29708956ff9296ceea263ee028c74704
codebrewers <codebrewers4@gmail.com>
2024-02-18 06:15:02 +0530

    final

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import AppRoutes from './AppRoutes';
 import Header from './components/Header/Header';
 import Loading from './components/Loading/Loading';
+
 import { useLoading } from './hooks/useLoading';
 import { setLoadingInterceptor } from './interceptors/loadingInterceptor';
 import { useEffect } from 'react';
-import Onboarding from './components/overboarding';
+// import Onboarding from './components/overboarding';
 function App() {
   const { showLoading, hideLoading } = useLoading();
 
